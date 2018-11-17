6fdef1b76bf007b873e5eea8347087d93233ad6b
Bijayash Chitrakar <bijayashchitrakar17@gmail.com>
2018-11-17 22:44:54 +0545

    developerpage

--- TBCErrorHander/TBCErrorHander/DeveloperPage.cs
+++ TBCErrorHander/TBCErrorHander/DeveloperPage.cs
@@ -12,9 +12,30 @@ namespace TBCErrorHander
 {
     public partial class DeveloperPage : Form
     {
+        public UserSessionModel session { get; set; }
         public DeveloperPage()
         {
             InitializeComponent();
+        }
+
+        public DeveloperPage(UserSessionModel usm)
+        {
+            InitializeComponent();
+            session = usm;
+        }
+
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            this.Hide();
+            f1.Show();
+        }
+
+        private void issueToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DevIssue di = new DevIssue(session);
+            di.MdiParent = this;
+            di.Show();
         }
     }
 }
